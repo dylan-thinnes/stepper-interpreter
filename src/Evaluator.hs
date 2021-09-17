a3ff8970b19da00c460b254894a4be59233de830
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-09-17 10:21:27 +0100

    Change indentation to 2 spaces

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -20,59 +20,59 @@ prexp qexp = [| putStrLn $(qexp >>= stringE . pprint) |]
 -- Evaluation
 
 data Evaluable
-    = EvalFunctionClauses [Clause] -- Dec.FunD
-    | EvalValueDeclaration Pat Body [Dec] -- Dec.ValD
-    | EvalExpression Exp -- Exp
+  = EvalFunctionClauses [Clause] -- Dec.FunD
+  | EvalValueDeclaration Pat Body [Dec] -- Dec.ValD
+  | EvalExpression Exp -- Exp
 
 type Environment = Map Name Evaluable
 
 definitions :: Exp -> [(Name, Evaluable)]
 definitions (LetE decs exp) = do
-    dec <- decs
-    case dec of
-        FunD name clauses -> pure (name, EvalFunctionClauses clauses)
-        ValD pat body decs -> do
-            _ <- undefined
-            []
-        _ -> []
+  dec <- decs
+  case dec of
+    FunD name clauses -> pure (name, EvalFunctionClauses clauses)
+    ValD pat body decs -> do
+      _ <- undefined
+      []
+    _ -> []
 
 data PatternMatch
-    = Success [(Name, Exp)]
-    | AwaitingApplication
-    | Failure [String]
-    deriving (Show)
+  = Success [(Name, Exp)]
+  | AwaitingApplication
+  | Failure [String]
+  deriving (Show)
 
 instance Semigroup PatternMatch where
-    -- Failures override AwaitingApplication or Successes
-    (<>) (Failure lMsgs) (Failure rMsgs) = Failure $ lMsgs ++ rMsgs
-    (<>) (Failure lMsgs) _ = Failure lMsgs
-    (<>) _ (Failure rMsgs) = Failure rMsgs
+  -- Failures override AwaitingApplication or Successes
+  (<>) (Failure lMsgs) (Failure rMsgs) = Failure $ lMsgs ++ rMsgs
+  (<>) (Failure lMsgs) _ = Failure lMsgs
+  (<>) _ (Failure rMsgs) = Failure rMsgs
 
-    -- AwaitingApplication overrides Successes
-    (<>) AwaitingApplication _ = AwaitingApplication
-    (<>) _ AwaitingApplication = AwaitingApplication
+  -- AwaitingApplication overrides Successes
+  (<>) AwaitingApplication _ = AwaitingApplication
+  (<>) _ AwaitingApplication = AwaitingApplication
 
-    -- Successful matches merge
-    (<>) (Success lMatches) (Success rMatches) = Success $ lMatches ++ rMatches
+  -- Successful matches merge
+  (<>) (Success lMatches) (Success rMatches) = Success $ lMatches ++ rMatches
 
 instance Monoid PatternMatch where
-    mempty = Success []
+  mempty = Success []
 
 flattenApps :: Exp -> (Exp, [Exp])
 flattenApps = fmap reverse . go
-    where
-        -- Build up argument list in reverse, to avoid O(n^2)
-        go (AppE f arg) = fmap (arg:) $ go f
-        go exp = (exp, [])
+  where
+    -- Build up argument list in reverse, to avoid O(n^2)
+    go (AppE f arg) = fmap (arg:) $ go f
+    go exp = (exp, [])
 
 matchPat :: Pat -> Exp -> PatternMatch
 matchPat (LitP pat) (LitE exp)
-    | pat == exp = Success []
-    | otherwise = Failure $ pure $ concat ["Literals unequal: pattern ", show pat, ", expression ", show exp, "."]
+  | pat == exp = Success []
+  | otherwise = Failure [concat ["Literals unequal: pattern ", show pat, ", expression ", show exp, "."]]
 matchPat (VarP name) exp = Success [(name, exp)]
 matchPat (TupP pats) (TupE exps)
-  | length pats /= length exps = Failure $ pure "Tuples are of different lengths."
-  | any isNothing exps = Failure $ pure "Cannot pattern match over partially applied tuples."
+  | length pats /= length exps = Failure ["Tuples are of different lengths."]
+  | any isNothing exps = Failure ["Cannot pattern match over partially applied tuples."]
   | otherwise = fold $ zipWith matchPat pats (catMaybes exps)
 matchPat (UnboxedTupP _) _ = error "matchPat: Unsupported pat UnboxedTupP"
 matchPat (UnboxedSumP _ _ _) _ = error "matchPat: Unsupported pat UnboxedSumP"
@@ -80,11 +80,11 @@ matchPat (ConP patConName pats) exp =
   let (f, args) = flattenApps exp
   in
   case f of
-      ConE expConName
-        | expConName /= patConName -> Failure $ pure "Pattern and expression have different constructor names."
-        | length pats /= length args -> Failure $ pure "Pattern and expression have different number of args to constructors - is the expression constructor partially applied?"
-        | otherwise -> fold $ zipWith matchPat pats args
-      _ -> Failure $ pure "Expression is not a data construction."
+    ConE expConName
+      | expConName /= patConName -> Failure ["Pattern and expression have different constructor names."]
+      | length pats /= length args -> Failure ["Pattern and expression have different number of args to constructors - is the expression constructor partially applied?"]
+      | otherwise -> fold $ zipWith matchPat pats args
+    _ -> Failure ["Expression is not a data construction."]
 -- matchPat (InfixP patL _ patR) _ = undefined
 matchPat (UInfixP patL _ patR) _ = error "matchPat: Unsupported pat UInfixP"
 matchPat (ParensP pat) exp = matchPat pat exp
@@ -95,11 +95,11 @@ matchPat (AsP name pat) exp = Success [(name, exp)] <> matchPat pat exp
 matchPat WildP _ = Success []
 -- matchPat (RecP _ fieldPats) _ = undefined
 matchPat (ListP pats) (ListE exps)
-  | length pats /= length exps = Failure $ pure "List pattern and list expression have different lengths."
+  | length pats /= length exps = Failure ["List pattern and list expression have different lengths."]
   | otherwise = fold $ zipWith matchPat pats exps
 matchPat (SigP pat type_) _ = error "matchPat: Unsupported pat SigP"
 matchPat (ViewP exp pat) _ = error "matchPat: Unsupported pat ViewP"
-matchPat _ _ = error "Pattern and expression do not match." -- TODO: Is this exhaustive enough?
+matchPat _ _ = Failure ["Pattern and expression do not match."] -- TODO: Is this exhaustive enough?
 
 patNames :: Pat -> [Name]
 patNames (LitP _) = []
