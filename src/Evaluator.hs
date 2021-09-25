76a13904b6d9b20c54f16f31af5c505f14555841
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-09-25 18:15:55 +0100

    Pre-annotate each node with its index using recursion scheme
    This avoids having to manually construct the index path in
    matchPatKeyed, which is a potential source of bugs.

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -16,12 +16,19 @@ module Evaluator where
 import "base" Control.Monad (zipWithM)
 import "base" Data.Bifunctor
 import "base" Data.Foldable (fold)
+import "base" Data.Functor.Compose
+import "base" Data.Functor.Const
+import "base" Data.Functor.Product
 import "base" Data.Maybe (isNothing, catMaybes, fromJust)
 import "base" Data.Void
 import "base" GHC.Generics
 
 import qualified "containers" Data.Map as M
 import           "containers" Data.Map (Map)
+
+import "deriving-compat" Text.Show.Deriving
+
+import "data-fix" Data.Fix (Fix(..))
 
 import "keys" Data.Key (Key(..), Keyed(..), keyed, Adjustable(..))
 
@@ -35,6 +42,9 @@ import Lift
 
 R.makeBaseFunctor ''Exp
 R.makeBaseFunctor ''Pat
+
+deriveShow1 ''ExpF
+deriveShow1 ''PatF
 
 -- Utils
 
@@ -57,6 +67,12 @@ projectK = keyed . R.project
 
 embedK :: (R.Corecursive t, Keyed (R.Base t)) => R.Base t (Key (R.Base t), t) -> t
 embedK = R.embed . dekeyed
+
+adjustRecursive
+  :: (Adjustable (R.Base t), R.Corecursive t, R.Recursive t)
+  => (t -> t) -> [Key (R.Base t)] -> t -> t
+adjustRecursive f [] t = f t
+adjustRecursive f (k:rest) t = R.embed $ adjust (adjustRecursive f rest) k $ R.project t
 
 zipConcatM :: Monad m => (a -> b -> m [c]) -> [a] -> [b] -> m [c]
 zipConcatM f as bs = concat <$> zipWithM f as bs
@@ -220,6 +236,12 @@ instance Adjustable ExpF where
 type PatKey = [Key PatF]
 type ExpKey = [Key ExpF]
 
+modPatByKey :: (Pat -> Pat) -> PatKey -> Pat -> Pat
+modPatByKey = adjustRecursive
+
+modExpByKey :: (Exp -> Exp) -> ExpKey -> Exp -> Exp
+modExpByKey = adjustRecursive
+
 data MatchFailure
   = Mismatch (PatKey, ExpKey) -- Pattern & expression both in WHNF and do not match - this pattern fails
   | NeedsReduction (PatKey, ExpKey) -- Specific subexpression needs further reduction due to given subpattern before pattern can be determined to match or fail
@@ -230,101 +252,113 @@ type MatchSuccess = [(Pat, Exp)]
 type MatchMonad a = Either MatchFailure a
 type MatchResult = MatchMonad MatchSuccess
 
-mismatch, needsReduction :: MatchMonad a
-mismatch = Left $ Mismatch ([], [])
-needsReduction = Left $ NeedsReduction ([], [])
-
-unexpectedError :: String -> MatchMonad a
-unexpectedError msg = Left $ UnexpectedError msg ([], [])
-
-prependMatchFailure :: (PatKey, ExpKey) -> MatchFailure -> MatchFailure
-prependMatchFailure (prePatKey, preExpKey) failure =
-  case failure of
-    UnexpectedError msg (patKey, expKey) -> UnexpectedError msg (prePatKey ++ patKey, preExpKey ++ expKey)
-    Mismatch (patKey, expKey) -> Mismatch (prePatKey ++ patKey, preExpKey ++ expKey)
-    NeedsReduction (patKey, expKey) -> NeedsReduction (prePatKey ++ patKey, preExpKey ++ expKey)
-
-withPreKeys :: (PatKey, ExpKey) -> MatchResult -> MatchResult
-withPreKeys prekeys = first $ prependMatchFailure prekeys
-
 listifyKey :: (a, b) -> ([a], b)
 listifyKey = first (\x -> [x])
 
 prependKey :: a -> ([a], b) -> ([a], b)
 prependKey a = first (a :)
 
+type Ann a f = Product (Const a) f
+type RecKey t = Ann [Key (R.Base t)] (R.Base t)
+
+annKeys :: (R.Recursive t, Keyed (R.Base t)) => t -> Fix (RecKey t)
+annKeys exp = R.ana go ([], exp)
+  where
+    go (prekeys, exp) = Pair (Const prekeys) (first (:prekeys) <$> projectK exp)
+
+deann :: R.Corecursive t => Fix (RecKey t) -> t
+deann = R.hoist (\(Pair _ tf) -> tf)
+
+deannWrapped :: R.Corecursive t => R.Base t (Fix (RecKey t)) -> t
+deannWrapped = R.embed . fmap deann
+
+toKeyPair :: Fix (Ann a f) -> (a, f (Fix (Ann a f)))
+toKeyPair (Fix (Pair (Const key) expf)) = (key, expf)
+
+toKeyPairDeann :: R.Corecursive t => Fix (RecKey t) -> ([Key (R.Base t)], t)
+toKeyPairDeann ann =
+  let (key, expf) = toKeyPair ann
+  in
+  (key, R.embed $ fmap deann expf)
+
 -- TODO: Handle infix applications & type applications (URGENT!)
 -- Could simplify as catamorphism w/ Compose (ExpKey,) ExpF ??
-flattenAppsKeyed :: ExpF (Key ExpF, Exp) -> ((ExpKey, Exp), [(ExpKey, Exp)])
+flattenAppsKeyed :: Fix (RecKey Exp) -> (Fix (RecKey Exp), [Fix (RecKey Exp)])
 flattenAppsKeyed = fmap reverse . go
   where
     -- Build up argument list in reverse, to avoid O(n^2)
-    go :: ExpF (Key ExpF, Exp) -> ((ExpKey, Exp), [(ExpKey, Exp)])
-    go (AppEF (fKey, f) (argKey, arg)) =
-      let (deepF, deepArgs) = go $ projectK f
-      in
-      (prependKey fKey deepF, ([argKey], arg) : map (prependKey fKey) deepArgs)
-    go exp = (([], embedK exp), [])
+    go :: Fix (RecKey Exp) -> (Fix (RecKey Exp), [Fix (RecKey Exp)])
+    go (Fix (Pair _ (AppEF func arg))) = (arg :) <$> go func
+    go exp = (exp, [])
 
 matchPatKeyed :: Pat -> Exp -> MatchResult
-matchPatKeyed pat exp = go (projectK pat) (projectK exp)
+matchPatKeyed pat exp = go (annKeys pat) (annKeys exp)
   where
-    rec = matchPatKeyed
+    go :: Fix (RecKey Pat) -> Fix (RecKey Exp) -> MatchResult
+    go annPat annExp = match patFAnn expFAnn
+      where
+        (patKey, patFAnn) = toKeyPair annPat
+        (expKey, expFAnn) = toKeyPair annExp
 
-    matchWithPreKeys :: (PatKey, Pat) -> (ExpKey, Exp) -> MatchResult
-    matchWithPreKeys (prePatKey, pat) (preExpKey, exp) =
-      withPreKeys (prePatKey, preExpKey) $ rec pat exp
+        mismatch, needsReduction :: MatchMonad a
+        mismatch = Left $ Mismatch (patKey, expKey)
+        needsReduction = Left $ NeedsReduction (patKey, expKey)
 
-    go :: PatF (Key PatF, Pat) -> ExpF (Key ExpF, Exp) -> MatchResult
-    go (LitPF pat) (LitEF exp)
-      | pat == exp = Right []
-      | otherwise = mismatch
-    go (VarPF name) exp = Right [(VarP name, embedK exp)]
-    go (TupPF pats) (TupEF mexps)
-      | length pats /= length mexps = unexpectedError "Tuple pattern-expression arity mismatch."
-      | otherwise = do
-          -- If there are any Nothing expressions, the tuple expression is partially applied and we short circuit with an error
-          let onlyJust Nothing = unexpectedError "Tuple expression is not fully applied (e.g. is a partially applied tuple like (,,) or (1,))."
-              onlyJust (Just a) = pure a
-          exps <- traverse onlyJust mexps
-          zipConcatM matchWithPreKeys (listifyKey <$> pats) (listifyKey <$> exps)
-    go (UnboxedTupPF _) _ = error "matchPatKeyed: Unsupported UnboxedTupP pattern in AST"
-    go (UnboxedSumPF _ _ _) _ = error "matchPatKeyed: Unsupported UnboxedSumP pattern in AST"
-    go (ConPF patConName pats) exp
-      | ((_, ConE expConName), args) <- flattenAppsKeyed exp
-      = if expConName /= patConName
-          then mismatch -- if constructors are different, assume that they belong to the same ADT & are mismatched
-                        -- TODO: How does this interact with PatternSynonyms?
-               -- if the pattern & expression constructors come from different ADTS, we'd return: `unexpectedError "Pattern and expression have different constructor names."`
-               -- alas, we can't do this comparison here, for now
-          else case compare (length pats) (length args) of
-            LT -> unexpectedError "Data constructor in expression is applied to too many arguments."
-            GT -> unexpectedError "Data constructor in expression isn't fully applied."
-            EQ -> zipConcatM matchWithPreKeys (listifyKey <$> pats) args
-    go (InfixPF patL _ patR) _ = error "matchPatKeyed: Unsupported pat InfixP" -- TODO: Urgently need to support this for cons
-    go (UInfixPF patL _ patR) _ = error "matchPatKeyed: Unsupported pat UInfixP"
-    go (ParensPF (patKey, pat)) exp = matchWithPreKeys ([patKey], pat) ([], embedK exp)
-    go pat (ParensEF (expKey, exp)) = matchWithPreKeys ([], embedK pat) ([expKey], exp)
-    go (TildePF (patKey, pat)) exp = Right [(pat, embedK exp)] -- lazy patterns always match, deferred to a "let-desugaring"
-    go (BangPF pat) exp = error "matchPatKeyed: Unsupported pat BangP"
-    go (AsPF name pat) exp = do
-      submatches <- matchWithPreKeys (listifyKey pat) ([], embedK exp)
-      pure $ (VarP name, embedK exp) : submatches
-    go WildPF _ = Right []
-    go (RecPF _ fieldPats) _ = error "matchPatKeyed: Unsupported pat RecP" -- TODO: Urgently need to support field patterns
-    go (ListPF pats) (ListEF exps)
-      | length pats /= length exps = unexpectedError "List pattern and list expression have different lengths."
-      | otherwise = zipConcatM matchWithPreKeys (listifyKey <$> pats) (listifyKey <$> exps)
-    go (SigPF pat type_) _ = error "matchPatKeyed: Unsupported pat SigP"
-    go (ViewPF exp pat) _ = error "matchPatKeyed: Unsupported pat ViewP"
-    go pat exp =
-      -- TODO: Definitely unfinished cases here somewhere...
-      let ((_, f), args) = flattenAppsKeyed exp
-      in
-      if
-        | length args == 0 -> mismatch
-        | (ConE _) <- f -> mismatch
-        -- | (VarE fName) <- f
-        -- , show fName == "GHC.Err.error"
-        -- -> Failure ["Pattern forces the evaluation of an error."]
-        | otherwise -> needsReduction -- TODO: Consider how caller checks for forcing of an `error "msg"`
+        unexpectedError :: String -> MatchMonad a
+        unexpectedError msg = Left $ UnexpectedError msg (patKey, expKey)
+
+        match :: PatF (Fix (RecKey Pat)) -> ExpF (Fix (RecKey Exp)) -> MatchResult
+        match (LitPF pat) (LitEF exp)
+          | pat == exp = Right []
+          | otherwise = mismatch
+        match (VarPF name) exp = Right [(VarP name, deannWrapped exp)]
+        match (TupPF pats) (TupEF mexps)
+          | length pats /= length mexps = unexpectedError "Tuple pattern-expression arity mismatch."
+          | otherwise = do
+              -- If there are any Nothing expressions, the tuple expression is partially applied and we short circuit with an error
+              let onlyJust :: Maybe a -> MatchMonad a
+                  onlyJust Nothing = unexpectedError "Tuple expression is not fully applied (e.g. is a partially applied tuple like (,,) or (1,))."
+                  onlyJust (Just a) = pure a
+              exps <- traverse onlyJust mexps
+              zipConcatM go pats exps
+        match (UnboxedTupPF _) _ = error "matchPatKeyed: Unsupported UnboxedTupP pattern in AST"
+        match (UnboxedSumPF _ _ _) _ = error "matchPatKeyed: Unsupported UnboxedSumP pattern in AST"
+        match (ConPF patConName pats) _
+          | (func, args) <- flattenAppsKeyed annExp
+          , (ConE expConName) <- deann func
+          = if expConName /= patConName
+              then mismatch -- if constructors are different, assume that they belong to the same ADT & are mismatched
+                            -- TODO: How does this interact with PatternSynonyms?
+                   -- if the pattern & expression constructors come from different ADTS, we'd return: `unexpectedError "Pattern and expression have different constructor names."`
+                   -- alas, we can't do this comparison here, for now
+              else case compare (length pats) (length args) of
+                LT -> unexpectedError "Data constructor in expression is applied to too many arguments."
+                GT -> unexpectedError "Data constructor in expression isn't fully applied."
+                EQ -> zipConcatM go pats args
+        match (InfixPF patL _ patR) _ = error "matchPatKeyed: Unsupported pat InfixP" -- TODO: Urgently need to support this for cons
+        match (UInfixPF patL _ patR) _ = error "matchPatKeyed: Unsupported pat UInfixP"
+        match (ParensPF pat) exp = go pat annExp
+        match pat (ParensEF exp) = go annPat exp
+        match (TildePF pat) exp = Right [(deann pat, deannWrapped exp)] -- lazy patterns always match, deferred to a "let-desugaring"
+        match (BangPF pat) exp = error "matchPatKeyed: Unsupported pat BangP"
+        match (AsPF name pat) exp = do
+          submatches <- go pat annExp
+          pure $ (VarP name, deannWrapped exp) : submatches
+        match WildPF _ = Right []
+        match (RecPF _ fieldPats) _ = error "matchPatKeyed: Unsupported pat RecP" -- TODO: Urgently need to support field patterns
+        match (ListPF pats) (ListEF exps)
+          | length pats /= length exps = unexpectedError "List pattern and list expression have different lengths."
+          | otherwise = zipConcatM go pats exps
+        match (SigPF pat type_) _ = error "matchPatKeyed: Unsupported pat SigP"
+        match (ViewPF exp pat) _ = error "matchPatKeyed: Unsupported pat ViewP"
+        match pat exp =
+          -- TODO: Definitely unfinished cases here somewhere...
+          let (f, args) = flattenAppsKeyed annExp
+          in
+          if
+            | length args == 0 -> mismatch
+            | (ConE _) <- deann f -> mismatch
+            -- | (VarE fName) <- f
+            -- , show fName == "GHC.Err.error"
+            -- -> Failure ["Pattern forces the evaluation of an error."]
+            | otherwise -> needsReduction -- TODO: Consider how caller checks for forcing of an `error "msg"`
