8f9009ca1103529089eed7ee6cbfda7ab1e689af
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-11-20 23:41:31 +0000

    Add more examples, add getLine waiting & logging to Evaluator.Demo

--- src/Demos/Evaluator.hs
+++ src/Demos/Evaluator.hs
@@ -60,12 +60,57 @@ testConstructorsInListMap = $(lift =<< [|
   map g [Just 1, Nothing, Just 2, Nothing, Just 3]
   |])
 
+testFilter :: Exp
+testFilter = $(lift =<< [|
+  let g 0 = False
+      g x = True
+      filter f (x:xs) = if f x then x : filter f xs else filter f xs
+      filter f [] = []
+  in
+  filter g [1,0,2,0,3,0]
+  |])
+
+testMapMaybe :: Exp
+testMapMaybe = $(lift =<< [|
+  let g 0 = Nothing
+      g x = Just (x * x)
+      mapMaybe f (x:xs) =
+        case f x of
+          Nothing -> mapMaybe f xs
+          Just y -> y : mapMaybe f xs
+      mapMaybe f [] = []
+  in
+  mapMaybe g [1,0,2,0,3,0]
+  |])
+
+testMapAndFilter :: Exp
+testMapAndFilter = $(lift =<< [|
+  let g 0 = False
+      g x = True
+      filter f (x:xs) = if f x then x : filter f xs else filter f xs
+      filter f [] = []
+      h x = x * x
+      map f (x:xs) = f x : map f xs
+      map f [] = []
+  in
+  map h (filter g [1,0,2,0,3,0])
+  |])
+
+prog :: Exp
+prog = $(lift =<< [|
+  let f y = Just y
+  in
+  case f y of
+    Nothing -> 0
+    Just y -> y
+  |])
+
 run :: Exp -> IO ()
 run exp = do
   printExp exp
   mapM_ printExp $ map unwrapReductionResult reductionSteps
   where
-  run = runInstantLog . handle  defaultEnvironment
+  run = runIdentity . handle  defaultEnvironment
   steps = iterate (run . unwrapReductionResult) (run exp)
   reductionSteps = takeWhile (not . isCannotReduce) steps
   printExp x = do
@@ -73,3 +118,4 @@ run exp = do
     let source = P.pprint $ P.removeBaseQualifications x
     highlighted <- readProcess "/usr/bin/batcat" (words "--theme zenburn -l haskell -pp --color always -") source
     putStr highlighted
+    getLine
